07cad936ac8b13869e3644224767b8d700003c52
VigeteX <vova.pel.vp@gmail.com>
2025-08-29 17:38:45 +0300

    test8

--- test/specs/test8.js
+++ test/specs/test8.js
@@ -1,7 +1,4 @@
-import { browser, expect } from '@wdio/globals'
 import assert from 'assert'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
 
 describe('My Login application', () => {    
     it('should login with valid credentials', async () => {
